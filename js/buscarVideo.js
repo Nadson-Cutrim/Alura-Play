ea20227dc54cf909a61e283f7a3c427f6cdfe2fb
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-27 23:17:38 -0300

    Busca não encontrada

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -2,20 +2,37 @@ import { conectaApi } from "./conectaApi.js";
 import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
-    evento.preventDefault();
-    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
-    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
+  evento.preventDefault();
+  const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+  const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    const lista = document.querySelector("[data-lista]");
+  const lista = document.querySelector("[data-lista]");
 
-    while (lista.firstChild) {
-        lista.removeChild(lista.firstChild);
-    }
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
 
-    busca.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+  busca.forEach((elemento) =>
+    lista.appendChild(
+      constroiCard(
+        elemento.titulo,
+        elemento.descricao,
+        elemento.url,
+        elemento.imagem
+      )
+    )
+  );
+  if (busca.length == 0) {
+    lista.innerHTML = `<h2 class="mensagem__titulo" style="color: red;">Não existe videos com esse termo 😭😭😭</h2>`;
+    lista.style = `display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    width: 100%;
+    height: 80vh;
+    `;
+  }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
 
-botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
+botaoDePesquisa.addEventListener("click", (evento) => buscarVideo(evento));
