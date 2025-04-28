7abea52997c034faaec86b8267817576f7092783
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-27 23:18:03 -0300

    carregamento da lista

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -3,37 +3,43 @@ import { conectaApi } from "./conectaApi.js";
 const lista = document.querySelector("[data-lista]");
 
 export default function constroiCard(titulo, descricao, url, imagem) {
-    const video = document.createElement("li");
-    video.className = "videos__item";
-    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+  const video = document.createElement("li");
+  video.className = "videos__item";
+  video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
     title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
 <div class="descricao-video">
-    <img src="${image}" alt="logo canal alura">
+    <img src="${imagem}" alt="logo canal alura">
     <h3>${titulo}</h3>
     <p>${descricao}</p>
-</div>`
+</div>`;
 
-    return video;
+  return video;
 }
 
 async function listaVideos() {
-    try{
+  try {
     const listaApi = await conectaApi.listaVideos();
-    listaApi.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
-} catch{
-    lista.innerHTML = `<h2 class="mensagem__titulo" style="color: #ff0000; font-size: 2rem;"> Não foi possivel carregar a lista de videos.</h2>`
-    lista.style = 
-    `display: flex; 
+    listaApi.forEach((elemento) =>
+      lista.appendChild(
+        constroiCard(
+          elemento.titulo,
+          elemento.descricao,
+          elemento.url,
+          elemento.imagem
+        )
+      )
+    );
+  } catch {
+    lista.innerHTML = `<h2 class="mensagem__titulo" style="color: #ff0000; font-size: 2rem;"> Não foi possivel carregar a lista de videos.</h2>`;
+    lista.style = `display: flex; 
     justify-content: center; 
     align-items: center; 
     width: 100%;
     height: 80vh;
-    `
-    
-}
+    `;
+  }
 }
 
 listaVideos();
